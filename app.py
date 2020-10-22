1f9cbef6d163b92db48a3763ab33096a0891abfa
rmotta1992 <68138899+rmotta1992@users.noreply.github.com>
2020-10-21 20:18:54 -0400

    add first code for jsonified data of precip data for api

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+
 from flask import Flask, jsonify
+
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+measurement = Base.classes.measurement
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return (
-        f"Welcome to the Justice League API!<br/>"
+        f"Welcome to the weather data homepage!<br/>"
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitaion<br/>"
         f"/api/v1.0/stations<br/>"
@@ -13,5 +28,29 @@ def home():
       #  f"/api/v1.0/""start""<br/>"
        # f"/api/v1.0/""start"/"end""<br/>"
     )
+@app.route("/api/v1.0/precipitaion")
+def precipition():
+    session = Session(engine)
+ 
+    results = session.query(measurement.prcp, measurement.date).all()
+
+    session.close()
+
+    prcp_data = []
+    for prcp, date in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        
+        prcp_data.append(prcp_dict)
+
+    return jsonify(prcp_data)
+
+
+
+       
+    
+   
+    
 if __name__ == "__main__":
     app.run(debug=True)
